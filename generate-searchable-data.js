6ba20740b9072165fbced513590fd79e89faa0ff
dargaCode <darga.code@gmail.com>
2018-07-25 17:54:13 -0700

    Generalize from product to entries

--- generate-searchable-data.js
+++ generate-searchable-data.js
@@ -7,41 +7,45 @@ const PATHS = {
   PROCESSED_DATA_JSON: './data/data-searchable.json',
 }
 
+// does the array have a key name?
+// set to null if not.
+const ENTRY_ARRAY_KEY = 'entries';
+
 const STARTING_ID = 10000;
 
 // DEPENDENCIES
 
-const productData = require(PATHS.RAW_DATA_JSON).products;
+const jsonData = require(PATHS.RAW_DATA_JSON);
 const Trie = require(PATHS.TRIE_CLASS);
 const fs = require('fs');
 
 // FUNCTIONS
 
-function processData(productEntries) {
-  const productNameSet = new Set()
-  const productTrie = new Trie();
-  const productDict = {};
+function processEntries(entries) {
+  const entryNameSet = new Set()
+  const entryTrie = new Trie();
+  const entryDict = {};
   let id = STARTING_ID;
 
-  for (const entry of productEntries) {
-    // deduplicate products
-    if (!productNameSet.has(entry.name)) {
-      productNameSet.add(entry.name);
+  for (const entry of entries) {
+    // deduplicate entries
+    if (!entryNameSet.has(entry.name)) {
+      entryNameSet.add(entry.name);
 
       // id is used to reduce duplicated data in the trie
       entry.id = ++id;
       // store the entry's id in a trie by its display name for fast lookup.
-      storeEntryInTrie(entry, productTrie);
+      storeEntryInTrie(entry, entryTrie);
       // store the entry's other data in a dict by its id, to help keep the trie as small as possible.
-      storeEntryInDict(entry, productDict);
+      storeEntryInDict(entry, entryDict);
     }
   }
 
   // combine both structures into single json, for fewer requests
   const processedObj = {
     // Trie only surfaces a jsonString, not references to its actual nodes.
-    trie: JSON.parse(productTrie.getJsonString()),
-    dict: productDict,
+    trie: JSON.parse(entryTrie.getJsonString()),
+    dict: entryDict,
   };
 
   return processedObj;
@@ -64,12 +68,13 @@ function storeEntryInDict(entry, dict) {
 function saveProcessedData(outputObj) {
   const jsonString = JSON.stringify(outputObj);
 
+  console.log('Saved processed data JSON...\n');
   fs.writeFile(PATHS.PROCESSED_DATA_JSON, jsonString, function (err) {
     if (err) {
       console.log(err);
     } else {
-      console.log('Saved processed data JSON:\n');
       console.log(jsonString);
+      console.log(`\n${PATHS.PROCESSED_DATA_JSON} saved!`);
     }
   });
 }
@@ -77,7 +82,12 @@ function saveProcessedData(outputObj) {
 // MAIN
 
 (function main() {
-  const processedDataObj = processData(productData);
+  // if there a key name for the top level array?
+  const entryData = ENTRY_ARRAY_KEY ?
+    jsonData[ENTRY_ARRAY_KEY] :
+    jsonData;
+
+  const processedDataObj = processEntries(entryData);
 
   saveProcessedData(processedDataObj);
 }());
